68c72104c37f157144abc33b27a3a009bcbdf06d
ZakPorat0 <zakporat2023@gmail.com>
2024-06-28 20:47:10 +0100

    Feat: add layout style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,19 +12,23 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/" element={<Hero />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
+        <div className="min-h-screen bg-gray-50">
+          <Navbar />
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <Routes>
+                <Route path="/auth" element={<Auth />} />
+                <Route path="/" element={<Hero />} />
+                <Route
+                  path="/dashboard"
+                  element={
+                    <ProtectedRoute>
+                      <Dashboard />
+                    </ProtectedRoute>
+                  }
+                />
+              </Routes>
+            </div>
+        </div>
       </Router>
       <Toaster />
     </AuthProvider>
